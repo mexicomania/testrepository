9cc260e191ee242a5a8c633f478454765a4626b9
Your Name <you@example.com>
2018-12-18 20:46:47 -0500

    n

--- front-end/src/utilities/utilities.js
+++
@@ -1,8 +0,0 @@
-
-export const apiUrl = 'https://visaapp1.herokuapp.com/api';
-
-export const readToken = () => JSON.parse(localStorage.getItem('token'));
-
-export const writeToken = (token) => localStorage.setItem('token',token);
-
-export const deleteToken = () => localStorage.removeItem('token');
